0edf8f4c009277e31a5fde9ae7efaab44ad018e7
Nikita PB <npb.nikita@gmail.com>
2018-10-15 14:17:54 +0300

    Add err 404 screen for incorrect routes

--- index.js
+++ index.js
@@ -3,22 +3,39 @@ const moment = require(`moment`);
 const fs = require(`fs`);
 
 const PORT = 8000;
+const DATA_FS_PATH = `./data/events.json`;
+const routes = {
+  ROOT: `/`,
+  STATUS: `/status`,
+  EVENTS_DATA: `/api/events`
+};
+
 const app = express();
 const startTime = Date.now();
-const eventsJSONfile = JSON.parse(fs.readFileSync(`./data/events.json`, `utf8`)); // Синхронно заводим переменную для файла с данными
+const eventsJSONfile = JSON.parse(fs.readFileSync(DATA_FS_PATH, `utf8`)); // Синхронно заводим переменную для файла с данными
 
-app.get(`/`, (request, response) => {
-  response.send(`Express server is up and running`)
+app.get(routes.ROOT, (request, response) => {
+  response.send(`<h1>Express server is up and running 👌</h1>`)
 });
 
-app.get(`/status`, (request, response) => {
-  const seconds = Date.now() - startTime;
-  const time = moment(seconds).utc().format(`HH:mm:ss`);
-  response.send(time);
+app.get(routes.STATUS, (request, response) => {
+  const timeDiff = Date.now() - startTime;
+  const serverStartTimer = {
+    timeFromServerStart: moment(timeDiff).utc().format(`HH:mm:ss`),
+  };
+  response.send(serverStartTimer);
 });
 
-app.get(`/api/events`, (request, response) => {
+app.get(routes.EVENTS_DATA, (request, response) => {
   response.send(eventsJSONfile);
+});
+
+// Отдает 404, если путь отличен от routes
+app.use((request, response) => {
+  response.status(404).send(`
+    <h1>Page not found</h1>
+    <br>
+    <span>Error 404</span>`);
 });
 
 app.listen(PORT, (err) => {
